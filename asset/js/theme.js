8246920cc8e398d531fe830d11cb0d70194e0a90
BlasterWhite <icarambala@gmail.com>
2021-03-18 20:04:36 +0100

    0.0.1

---
+++ asset/js/theme.js
@@ -0,0 +1,13 @@
+
+console.log("[INFORAMTION] \"theme.js\" chargé.")
+
+function test() {
+  console.log("Reçu 5/5");
+}
+
+function switch_theme() {
+   var element = document.body;
+   element.classList.toggle("white-mode");
+    var logo = document.getElementById("logo");
+   logo.classList.toggle("white-logo-mode");
+}
